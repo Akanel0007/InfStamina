f3101d89b029f690ba469b37845f17fcd8678312
Akanel0007 <128928886+Akanel0007@users.noreply.github.com>
2023-04-25 13:43:30 +0300

    Add files via upload

---
+++ CgInfStamina/CgInfStamina/CgInfStamina.cs
@@ -0,0 +1,47 @@
+﻿using Rocket.API;
+using Rocket.Core.Logging;
+using Rocket.Core.Plugins;
+using Rocket.Unturned.Events;
+using Rocket.Unturned.Player;
+using SDG.Unturned;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace CodeGeassPluginsInfStamina
+{
+    public class Infstamina : RocketPlugin<CgInfStaminaConfig>
+    {
+        protected override void Load()
+        {
+
+            Logger.Log($"{name} {Assembly.GetName().Version} Loaded");
+            UnturnedPlayerEvents.OnPlayerUpdateStamina += geassstamina;
+        }
+
+        private void geassstamina(UnturnedPlayer player, byte stamina)
+        {
+            if (!player.HasPermission(Configuration.Instance.geassstaminaperm))
+            {
+                return;
+            }
+
+            if (stamina >= 30)
+            {
+                return;
+            }
+            player.Player.life.serverModifyStamina(100);
+        }
+
+
+        protected override void Unload()
+        {
+            Logger.Log($"{Name} Unloaded");
+        }
+    }
+}
---
+++ CgInfStamina/CgInfStamina/CgInfStaminaConfiguraiton.cs
@@ -0,0 +1,22 @@
+﻿using Rocket.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeGeassPluginsInfStamina
+{
+    public class CgInfStaminaConfig : IRocketPluginConfiguration
+    {
+        public string LoadMessage { get; set; }
+        public string geassstaminaperm;
+        public void LoadDefaults()
+        {
+            geassstaminaperm = "geassstperm";
+            LoadMessage = "Code Geass Support https://discord.gg/HCfG6JctPC";
+        }
+
+    }
+
+}
